cd6f3c61d9faba6c2008c72d374f4112f95fb4bb
Ale Mizandari <mizandarichannel@gmail.com>
2025-04-07 22:31:36 +0400

    CA regex addedd

--- telegram_monitor.py
+++ telegram_monitor.py
@@ -19,19 +19,29 @@ async def send_message_to_channels(message_text):
     for post_channel_id in post_channel_ids:
         await client.send_message(post_channel_id, message_text)
 
+async def analyze_message(message_text):
+    solana_memecoin_regex = re.compile(r'([1-9A-HJ-NP-Za-km-z]{32,44})')
+
+    match = solana_memecoin_regex.search(message_text)
+    if match:
+        address = match.group(1)
+        return address
+
 @client.on(events.NewMessage(chats=channel_ids))
 async def handler(event):
     channel_id = event.chat_id
     message_text = event.message.message    
     user_id = event.sender_id
 
-    if str(channel_id).lstrip("-100") in track_users:
-        if user_id in track_users[str(channel_id).lstrip("-100")]:
-            await send_message_to_channels("tracked_user")
+    ca_address = await analyze_message(message_text)
+    if ca_address:
+        if str(channel_id).lstrip("-100") in track_users:
+            if user_id in track_users[str(channel_id).lstrip("-100")]:
+                await send_message_to_channels(ca_address)
+            else:
+                return
         else:
-            return
-    else:
-        await send_message_to_channels("tracked_user")
+            await send_message_to_channels(ca_address)
 
 async def main():
     await client.start(phone=phone)
